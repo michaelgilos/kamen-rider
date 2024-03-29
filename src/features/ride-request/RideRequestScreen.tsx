7a905fe3b0640c7a07079ab513a577938dca2af7
Michael Gilos <gilosmichael@gmail.com>
2024-03-29 14:51:25 +0800

    create navigation screens for Home and RideRequest

---
+++ src/features/ride-request/RideRequestScreen.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+export const RideRequestScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Ride Request Screen</Text>
+    </View>
+  );
+};
