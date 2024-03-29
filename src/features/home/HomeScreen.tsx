0d0630172acc15588e1d5e45f9dca91882180cbd
Michael Gilos <gilosmichael@gmail.com>
2024-03-29 14:57:12 +0800

    show mapview

--- src/features/home/HomeScreen.tsx
+++ src/features/home/HomeScreen.tsx
@@ -1,10 +1,21 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 
 export const HomeScreen = () => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ color: "#000" }}>Home Screen</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
