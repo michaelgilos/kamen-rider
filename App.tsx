a924e114e35e3aba90067bdf53e16deb56c27d0e
Michael Gilos <gilosmichael@gmail.com>
2024-03-29 14:54:54 +0800

    remove view parent to show home screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StyleSheet, View } from "react-native";
 import { HomeScreen } from "./src/features/home/HomeScreen";
 import { RideRequestScreen } from "./src/features/ride-request/RideRequestScreen";
 
@@ -8,22 +7,11 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="RideRequest" component={RideRequestScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="RideRequest" component={RideRequestScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- src/features/home/HomeScreen.tsx
+++ src/features/home/HomeScreen.tsx
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export const HomeScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <Text style={{ color: "#000" }}>Home Screen</Text>
     </View>
   );
 };
