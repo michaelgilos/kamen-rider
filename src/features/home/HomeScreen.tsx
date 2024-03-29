98078f935100d6c3afff50b05609b23e5d32be27
Michael Gilos <gilosmichael@gmail.com>
2024-03-29 15:13:53 +0800

    add expo location to get rider's current location

--- src/features/home/HomeScreen.tsx
+++ src/features/home/HomeScreen.tsx
@@ -1,8 +1,33 @@
-import * as React from "react";
-import { View, StyleSheet } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+
+import * as Location from 'expo-location';
+import {LocationAccuracy, LocationObject} from 'expo-location';
 
 export const HomeScreen = () => {
+  const [location, setLocation] = useState<LocationObject | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const {status} = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync({
+        accuracy: LocationAccuracy.Balanced,
+        timeInterval: 100,
+      });
+      setLocation(location);
+    })();
+  }, []);
+
+  if (!location) {
+    return <Text>Failed to load user location</Text>;
+  }
+
   return (
     <View style={styles.container}>
       <MapView
@@ -13,6 +38,12 @@ export const HomeScreen = () => {
         zoomTapEnabled={true}
         maxZoomLevel={60}
         provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
       />
     </View>
   );
@@ -23,7 +54,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   map: {
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '100%',
   },
 });
